bbf21b0c27e912172e9ed4e91324ec7582e10718
Laxmi chaudhary <166985345+Laxmi787@users.noreply.github.com>
2024-07-08 22:01:08 +0530

    file added

---
+++ Calculator.py
@@ -0,0 +1,23 @@
+print ("------Mini Calculator-------")
+
+num1=float(input("Enter a first number here:  "))
+num2=float(input("Enter a second number here:  "))
+
+print ("press 1 for addition \npress 2 for subtraction \npress 3 for multiplication \npress 4 for division")
+
+choice = int(input("Enter your choice from 1-4: "))
+
+if choice == 1:
+    print("The Addition of given two numbers is:", num1+num2)
+
+elif choice == 2:
+    print("The Subtraction of given two numbers is:",num1-num2)
+    
+elif choice == 3:
+    print("The Multipication of given two numbers is:",num1*num2)
+
+elif choice == 4:
+    print("The Division of given two numbers is:",num1/num2)
+
+else:
+    print ("Invalid Input...")
